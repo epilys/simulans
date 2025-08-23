39759995dda7d25a61bf81c97051c0f2e197dc92
Manos Pitsidianakis <manos@pitsidianak.is>
2025-08-23 16:52:43 +0300

    test_kernel: remove platform cfg opt
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- test_kernel/build.rs
+++ test_kernel/build.rs
@@ -1,21 +1,18 @@
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(platform, values(\"qemu\"))",);
+    println!("cargo::rerun-if-changed=entry.S");
+    println!("cargo::rerun-if-changed=exceptions.S");
+    println!("cargo::rerun-if-changed=idmap_qemu.S");
+    println!("cargo::rerun-if-changed=image.ld");
 
     std::env::set_var("CROSS_COMPILE", "aarch64-none-elf");
     std::env::set_var("CC", "clang");
 
-    let platform = std::env::var("CARGO_CFG_PLATFORM").expect("Missing platform name");
-    assert_eq!(
-        platform.as_str(),
-        "qemu",
-        "Unexpected platform name {:?}. Supported platforms: \"qemu\"",
-        platform,
-    );
     cc::Build::new()
         .file("entry.S")
         .file("exceptions.S")
-        .file(format!("idmap_{platform}.S"))
+        .file(format!("idmap_qemu.S"))
         .compile("test_kernel");
-    println!("cargo:rustc-link-arg=-T{platform}.ld");
+    println!("cargo::rerun-if-changed=qemu.ld");
+    println!("cargo:rustc-link-arg=-Tqemu.ld");
     println!("cargo:rustc-link-arg=-Timage.ld");
 }
