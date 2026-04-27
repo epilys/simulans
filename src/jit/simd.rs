05a3d5d7da9339f0afef7da877124169ac38ee8e
Manos Pitsidianakis <manos@pitsidianak.is>
2026-04-27 11:27:34 +0300

    jit/simd: rewrite get_elem/set_elem
    Implementing CM{cnd} revealed problems with the implementation
    
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- src/jit/simd.rs
+++ src/jit/simd.rs
@@ -39,21 +39,70 @@ impl BlockTranslator<'_> {
 
     /// `J1.3.3.29 Elem: Elem[] - getter`
     pub fn get_elem(&mut self, simd_value: Value, e: i64, size: Width) -> Value {
-        let val = self.builder.ins().ushr_imm(simd_value, e * i64::from(size));
-        let value = self.builder.ins().band_imm(val, 2_i64.pow(size as u32 - 1));
+        if matches!(size, Width::_128) {
+            assert_eq!(e, 0);
+            return simd_value;
+        }
+        let mut value = self.builder.ins().ushr_imm(simd_value, e * i64::from(size));
+        if size < Width::_64 {
+            value = self
+                .builder
+                .ins()
+                .band_imm(value, bit_mask!(off = 0, len = size as u32));
+        }
         self.builder.ins().ireduce(size.into(), value)
     }
 
     /// `J1.3.3.29 Elem: Elem[] - setter`
     pub fn set_elem(&mut self, simd_value: Value, e: i64, size: Width, value: Value) -> Value {
-        let value = self.builder.ins().uextend(I128, value);
         let offset = e * i64::from(size);
-        // Clear previous element value.
-        let simd_value = self
-            .builder
-            .ins()
-            .band_imm(simd_value, !bit_mask!(off = offset, len = size as i64));
-        let value = self.builder.ins().ishl_imm(value, offset);
-        self.builder.ins().bor(simd_value, value)
+        if offset >= 64 || size >= Width::_64 {
+            let value = if size == Width::_64 {
+                value
+            } else {
+                self.builder.ins().uextend(I64, value)
+            };
+            let (lo, hi) = self.builder.ins().isplit(simd_value);
+            let (lo, hi) = if offset >= 64 {
+                let offset = offset - 64;
+                // Clear previous element value.
+                let hi = if matches!(size, Width::_64) {
+                    assert_eq!(offset, 0);
+                    self.builder.ins().iconst(I64, 0)
+                } else {
+                    self.builder
+                        .ins()
+                        .band_imm(hi, !bit_mask!(off = offset, len = size as i64))
+                };
+                let value = self.builder.ins().ishl_imm(value, offset);
+                (lo, self.builder.ins().bor(hi, value))
+            } else {
+                // Clear previous element value.
+                let lo = if matches!(size, Width::_64) {
+                    assert_eq!(offset, 0);
+                    self.builder.ins().iconst(I64, 0)
+                } else {
+                    self.builder
+                        .ins()
+                        .band_imm(lo, !bit_mask!(off = offset, len = size as i64))
+                };
+                let value = self.builder.ins().ishl_imm(value, offset);
+                (self.builder.ins().bor(lo, value), hi)
+            };
+
+            let simd_value = self.builder.ins().iconcat(lo, hi);
+            self.builder
+                .ins()
+                .bitcast(I128, super::MEMFLAG_LITTLE_ENDIAN, simd_value)
+        } else {
+            let value = self.builder.ins().uextend(I128, value);
+            // Clear previous element value.
+            let simd_value = self
+                .builder
+                .ins()
+                .band_imm(simd_value, !bit_mask!(off = offset, len = size as i64));
+            let value = self.builder.ins().ishl_imm(value, offset);
+            self.builder.ins().bor(simd_value, value)
+        }
     }
 }
